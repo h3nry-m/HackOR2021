3150ff22bc7d1623c5be064a87e2564bdb126449
Henry <liveoak35@gmail.com>
2021-03-26 17:15:26 -0700

    Added index and register view

--- HackOR/settings.py
+++ HackOR/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'calorie_counter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
