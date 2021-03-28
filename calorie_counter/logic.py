7db19888d0984c3f58dc87b5470fe3a5bc117d17
dclaiche <dclaiche@hotmail.com>
2021-03-28 21:53:28 +0200

    changes to var

--- calorie_counter/logic.py
+++ calorie_counter/logic.py
@@ -43,15 +43,10 @@ def recommended_calories(total_daily_energy_expenditure, goal, weight):
     # rate values
     half_per_week_rate = total_daily_energy_expenditure - 250
     one_per_week_rate = total_daily_energy_expenditure - 500
-    one_half_per_week_rate = total_daily_energy_expenditure - 750
-    two_per_week_rate = total_daily_energy_expenditure - 1000
     # array to hold the rate values
-    rate = [half_per_week_rate, one_per_week_rate,
-            one_half_per_week_rate, two_per_week_rate]
+    rate = [half_per_week_rate, one_per_week_rate]
     # calulating the time to reach your goal
     time_to_lose_half = need_to_lose/rate[0]
     time_to_lose_one = need_to_lose/rate[1]
-    time_to_lose_one_half = need_to_lose/rate[2]
-    time_to_lose_two = need_to_lose/rate[3]
     # return all the "time to lose" variables to spit back out
-    return rate, time_to_lose_half, time_to_lose_one, time_to_lose_one_half, time_to_lose_two
+    return rate, time_to_lose_half, time_to_lose_one
